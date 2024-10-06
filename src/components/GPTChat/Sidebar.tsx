fc9fd82acc9954cac0575f87ed2737b8c1d3d288
janst.geo <janst.geo@gmail.com>
2024-10-06 02:40:17 +0000

    front end ui sidebar for ai agents added

--- src/components/GPTChat/Sidebar.tsx
+++ src/components/GPTChat/Sidebar.tsx
@@ -1,5 +1,11 @@
 import logo from "../../assets/GreenshiftAiLogoCroped.png";
 import './style.css';
+import carbon from "../../assets/Carbon.png";
+import energy from "../../assets/Energy.png";
+import equipment from "../../assets/Equipment.png";
+import waste from "../../assets/Waste.png";
+
+
 
 const Sidebar = () => {
     return (
@@ -13,15 +19,63 @@ const Sidebar = () => {
                             <img src={logo} alt="Greenshift AI Logo" className="img-fluid" />
                         </span>
                         <div className="ml-2 mt-1">
-                            <small>New chat</small>
+                            <small>Overview</small>
                         </div>
                     </span>
                     
                 </div>
 
                 <div className="px-4 mt-3">
-                    <small className="text-secondary">Today</small>
+                    <small className="text-secondary">Agents</small>
                 </div>
+
+                <div className="gpt-new-chat-btn px-2 py-2 mt-2 mb-1 ml-2">
+                    <span className="d-flex">
+                        <span className="gpt-circle-2 text-dark">
+                            <img src={energy} alt="Greenshift AI Logo" className="img-fluid" />
+                        </span>
+                        <div className="ml-2 mt-1">
+                            <small>Energy Advisor</small>
+                        </div>
+                    </span>
+                    
+                </div>
+                <div className="gpt-new-chat-btn px-2 py-2 mt-2 mb-1 ml-2">
+                    <span className="d-flex">
+                        <span className="gpt-circle-2 text-dark">
+                            <img src={waste} alt="Greenshift AI Logo" className="img-fluid" />
+                        </span>
+                        <div className="ml-2 mt-1">
+                            <small>Waste Advisor</small>
+                        </div>
+                    </span>
+                    
+                </div>
+
+                <div className="gpt-new-chat-btn px-2 py-2 mt-2 mb-1 ml-2">
+                    <span className="d-flex">
+                        <span className="gpt-circle-2 text-dark">
+                            <img src={equipment} alt="Greenshift AI Logo" className="img-fluid" />
+                        </span>
+                        <div className="ml-2 mt-1">
+                            <small>Equipment Advisor</small>
+                        </div>
+                    </span>
+                    
+                </div>
+
+                <div className="gpt-new-chat-btn px-2 py-2 mt-2 mb-1 ml-2">
+                    <span className="d-flex">
+                        <span className="gpt-circle-2 text-dark">
+                            <img src={carbon} alt="Greenshift AI Logo" className="img-fluid" />
+                        </span>
+                        <div className="ml-2 mt-1">
+                            <small>Carbon Advisor</small>
+                        </div>
+                    </span>
+                    
+                </div>
+
 
             </div>
         </>
